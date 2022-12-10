397b7d7ba0de779da1767c77e20bf909dc233685
Yahor Barkouski <yahor.barkouski@gmail.com>
2022-12-11 00:47:25 +0300

    chore: migrate constants to core

---
+++ notion-toggler-core/src/main/kotlin/io/yahorbarkouski/notion/toggler/core/constants/Constants.kt
@@ -0,0 +1,5 @@
+package io.yahorbarkouski.notion.toggler.core.constants
+
+const val DEFAULT_DATABASE_NAME = "Features"
+const val BASE_FEATURE_FLAG_CLASS = "io.yahorbarkouski.notion.toggler.core.FeatureFlag"
+const val DEFAULT_REFRESH_INTERVAL = 30L
